4a4e2c3fdff901020d52def542196f858e91aba9
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-15 14:54:28 -0700

    Added the code for BinarySearch class and the accompanying search method

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,5 +1,44 @@
 class BinarySearch(list):
     """This class inherits from python's list class"""
-    def search(self, arg):
-        """This methods is for implementing the search algorithm"""
-        pass
+    def __init__(self, a, b):
+        
+        super(BinarySearch, self).__init__()
+
+        for number in range(1, a + 1):
+            self.append(number * b)
+
+        self.length = len(self)
+
+    def search(self, value):
+        
+        count = 0             #for counting the number of iterations
+        first  = 0            #Denotes the starting point
+        last = len(self) - 1  #Denotes the ending point
+        index = 0             #Index of the elements
+
+        if value == self[first]:
+            index = first
+            return {'count': count, 'index': index}
+
+        elif value == self[last]:
+            index = last
+            return {'count': count, 'index': index}
+        elif value not in self:
+            index = -1
+            return {'count': count, 'index': index}
+
+        while first <= last:
+        #Binary search
+            mid = (first + last)//2
+
+            if self[mid] == value:
+                index = mid
+                return {'count': count, 'index': index}
+            else:
+                count += 1
+                if value < self[mid]:
+                    last = mid - 1
+                else:
+                    first = mid + 1
+
+        return {'count': count, 'index': index}
